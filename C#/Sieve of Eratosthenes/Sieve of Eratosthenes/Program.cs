7fcf2211cfd11eb5ce856f5f5c5c171f53676b49
Karonis <19214686+KaronisInProgram@users.noreply.github.com>
2019-10-19 20:25:48 +0200

    Base Implementation

---
+++ C#/Sieve of Eratosthenes/Sieve of Eratosthenes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace SieveOfEratosthenes
+{
+    internal static class Program
+    {
+        #region Private Methods
+
+        public static void Main()
+        {
+            var sieve = new Sieve();
+
+            ReadOnlyCollection<int> result;
+
+            result = sieve.SetMaxnumber(2).Calculate();
+            Console.WriteLine(string.Join(", ", result));
+            result = sieve.SetMaxnumber(9).Calculate();
+            Console.WriteLine(string.Join(", ", result));
+            result = sieve.SetMaxnumber(10).Calculate();
+            Console.WriteLine(string.Join(", ", result));
+            result = sieve.SetMaxnumber(100).Calculate();
+            Console.WriteLine(string.Join(", ", result));
+
+            Console.ReadKey();
+        }
+
+        #endregion Private Methods
+    }
+}
---
+++ C#/Sieve of Eratosthenes/Sieve of Eratosthenes/Sieve.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace SieveOfEratosthenes
+{
+    internal class Sieve
+    {
+        #region Private Fields
+
+        private readonly int startNumber = 2;
+        private int maxNumber = 2;
+
+        #endregion Private Fields
+
+        #region Public Methods
+
+        public ReadOnlyCollection<int> Calculate()
+        {
+            IList<int> result = new List<int>();
+
+            // Fill List till max Number
+            for (int i = startNumber; i <= maxNumber; i++)
+            {
+                result.Add(i);
+            }
+
+            var squarRoot = Convert.ToInt32(Math.Sqrt(maxNumber));
+
+            for (int i = startNumber; i < squarRoot; i++)
+            {
+                result = result.Where((number) => (number % i) != 0).ToList();
+            }
+
+            return new ReadOnlyCollection<int>(result);
+        }
+
+        public Sieve SetMaxnumber(int maxNumber)
+        {
+            this.maxNumber = maxNumber;
+            return this;
+        }
+
+        #endregion Public Methods
+    }
+}
